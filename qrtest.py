61b136ea50d7cc5b21b39879bedb437a6ceca960
Ralfbrse <raphaeloburce@gmail.com>
2022-10-07 11:54:00 -0700

    Resized

--- qrtest.py
+++ qrtest.py
@@ -1,3 +1,5 @@
+
+from turtle import color
 import qrcode
 from tkinter import *
 from tkinter import ttk
@@ -5,11 +7,16 @@ from PIL import Image, ImageTk
 # Top level window
 root = Tk()
 root.title("QR code generator")
-root.geometry("700x700")
+root.geometry("400x250")
+
+
+# label
+inputLabel = Label(root, text="Insert link/message")
+inputLabel.grid(column=0, row=0, pady=2, padx=2)
 
 # text box
-inputtxt = Text(root, height=5, width=20)
-inputtxt.pack()
+inputtxt = Text(root, height=3, width=20)
+inputtxt.grid(column=0, row=1, pady=2, padx=2)
 
 
 def GenQR():
@@ -23,15 +30,15 @@ def GenQR():
     temp.save('./qrcode.GIF')
 
     image = Image.open('./qrcode.GIF')
-    resize_image = image.resize((300, 300))
+    resize_image = image.resize((200, 200))
     img = ImageTk.PhotoImage(resize_image)
 
-    outCode.create_image(300, 300, image=img)
+    outCode.create_image(100, 100, image=img)
 
 
 showCodeBtn = Button(root, text="Generate QR code", command=GenQR)
-showCodeBtn.pack()
+showCodeBtn.grid(column=0, row=2)
 
-outCode = Canvas(root, width=500, height=500)
-outCode.pack()
+outCode = Canvas(root, width=200, height=200)
+outCode.grid(column=1, row=0, columnspan=6, rowspan=6)
 root.mainloop()
